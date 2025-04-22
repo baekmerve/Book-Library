325f60d81c9cafc63b567346874aa7fbd000b03b
baekmerve <baekmerve@gmail.com>
2025-04-22 15:04:50 +0900

    metadata edit, favicon add

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,34 @@ const poetsen = Poetsen_One({
 })
 
 export const metadata: Metadata = {
-  title: 'Book Library',
-  description: 'A Book Library website',
+  title: 'Book Library | Discover and Manage Your Favorite Books',
+  description:
+    'Explore, borrow, and manage your favorite books in a beautifully designed digital library. Personalized account features, book search, and reminders included.',
+  keywords: [
+    'book library',
+    'digital library',
+    'online book management',
+    'borrow books',
+    'library system',
+    'book search',
+    'React book app',
+    'Next.js library project',
+  ],
+  authors: [{ name: 'Merve B.', url: 'https://www.baekmerve.website' }],
+  creator: 'Merve B.',
+  publisher: 'Book Library',
+  openGraph: {
+    title: 'Book Library | Your Digital Reading Companion',
+    description:
+      'A modern library system where you can browse, borrow, and manage books online. Built with Next.js and Tailwind CSS.',
+    url: 'https://book-library-silk-seven.vercel.app/',
+    siteName: 'Book Library',
+
+    locale: 'ko_KR',
+    type: 'website',
+  },
+
+  metadataBase: new URL('https://book-library-silk-seven.vercel.app'),
 }
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
